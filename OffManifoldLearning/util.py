6f482ecfd566aa5990a1b2d7180c9e7afe6b3e47
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2026-02-24 12:48:38 -0500

    Update util.py

--- OffManifoldLearning/util.py
+++ OffManifoldLearning/util.py
@@ -8,7 +8,7 @@ def calc_intrinsic_manifold(F: np.ndarray, d: int):
     return B
 
 
-def reshape_off_manifold(B_on, B_off, d, angle=.9):
+def reshape_off_manifold(B_on, B_off, d, angle=1):
     rng = np.random.default_rng()
     T = rng.standard_normal((B_off.shape[1], d))  # (q-d, d)
     T /= np.linalg.norm(T, axis=0, keepdims=True) + 1e-12
@@ -17,4 +17,46 @@ def reshape_off_manifold(B_on, B_off, d, angle=.9):
     B_reshaped = B_om[:, :d]
     return B_reshaped
 
+
+def curvature_2d(traj: np.ndarray, dt: float, eps: float = 1e-8, eps_pos: float = 1e-6):  # "almost no movement" per sample
+    x, y = traj[:, 0], traj[:, 1]
+
+    dx  = np.gradient(x, dt)
+    dy  = np.gradient(y, dt)
+    ddx = np.gradient(dx, dt)
+    ddy = np.gradient(dy, dt)
+
+    num = np.abs(dx * ddy - dy * ddx)
+    den = (dx*dx + dy*dy)**1.5 + eps
+    kappa = num / den
+
+    kappa_mean = kappa.mean()
+    kappa_max  = kappa.max()
+
+    return kappa, kappa_mean, kappa_max
+
+
+def calc_jerk(traj, dt):
+    """
+    traj : array (T, 2)  -> columns are x and y
+    dt   : time step
+    returns:
+        jerk      : (T, 2) jerk vector
+        jerk_mag  : (T,)   jerk magnitude
+    """
+    mask = ~np.isnan(traj[:, 0])
+
+    x = traj[mask, 0]
+    y = traj[mask, 1]
+
+    dx  = np.gradient(x, dt)
+    dy  = np.gradient(y, dt)
+
+    ddx = np.gradient(dx, dt)
+    ddy = np.gradient(dy, dt)
+
+    dddx = np.gradient(ddx, dt)
+    dddy = np.gradient(ddy, dt)
+
+    return np.sum(dddx**2 + dddy**2) * dt
 
